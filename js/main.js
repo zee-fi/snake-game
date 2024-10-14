3e5ecbd7bf7cac92980844dec45a2db2c7c5b4ce
fiona-eh <fionahulse@mac.com>
2024-10-14 13:17:50 +0200

    create initial game setup with board and player class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-console.log("hello");
+
+
+class Player {
+    constructor(){
+        this.width = 1;
+        this.height = 2;
+        this.positionX = 35 - this.width / 2;
+        this.positionY = 35 - this.height / 2;
+
+        this.createPlayerElement();
+    }
+
+    createPlayerElement() {
+        const domElementPlayer = document.createElement("div");
+
+        domElementPlayer.id = "player";
+        domElementPlayer.style.width = this.width + "vw";
+        domElementPlayer.style.height = this.height + "vh";
+        domElementPlayer.style.left = this.positionX + "vw";
+        domElementPlayer.style.bottom = this.positionY + "vh";
+
+        const board = document.getElementById("board");
+        board.appendChild(domElementPlayer);
+    }
+
+    moveRigt() {
+        this.positionX++;
+    }
+    moveLeft() {
+        this.positionX--;
+    }
+    moveUp() {
+        this.positionY++;
+    }
+    moveDown() {
+        this.positionY--;
+    }
+}
+
+const player = new Player();
+
+
