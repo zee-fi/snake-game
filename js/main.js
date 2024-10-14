048c7ca7ace1fff77b3533516327c5e16af38d1e
fiona-eh <fionahulse@mac.com>
2024-10-14 18:13:06 +0200

    create Food class and functionality for food to appear in random interval

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 class Player {
     constructor(){
-        this.width = 1.5;
+        this.width = 1;
         this.height = 2;
         this.positionX = 35 - this.width / 2;
         this.positionY = 35 - this.height / 2;
@@ -31,6 +31,10 @@ class Player {
     }
     moveLeft() {
         this.positionX--;
+        if (player.positionX < 0){
+        console.log("game over");
+        return
+        }
         this.domElementPlayer.style.left = this.positionX +"vw";
     }
     moveUp() {
@@ -39,6 +43,10 @@ class Player {
     }
     moveDown() {
         this.positionY--;
+        if (player.positionY < 0){
+            console.log("game over");
+            return
+        }
         this.domElementPlayer.style.bottom = this.positionY +"vh";
     }
 }
@@ -47,16 +55,64 @@ const player = new Player();
 
 
 document.addEventListener('keydown', (e) => {
-    if(e.code === 'ArrowLeft' || 'a'){
+    if(e.code === 'ArrowLeft'){
         player.moveLeft();
     }
-    else if(e.code === 'ArrowRight' || 'd'){
+    else if(e.code === 'ArrowRight'){
         player.moveRight();
     }
-    else if(e.code === 'ArrowUp' || 'w'){
+    else if(e.code === 'ArrowUp'){
         player.moveUp();
     }
-    else if(e.code === 'ArrowDown' || 's'){
+    else if(e.code === 'ArrowDown'){
         player.moveDown();
     }
 })
+
+
+/*function collisionWithBorder() {
+    if (player.positionX < 0 ||
+        player.positionY < 0
+    ){
+    console.log("game over")
+    }
+}*/
+
+
+
+
+
+class Food {
+    constructor(){
+        this.width = 1;
+        this.height = 2;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.domElementFood = null;
+        this.board = null;
+
+        this.createFoodElement();
+    }
+
+    createFoodElement() {
+        this.domElementFood = document.createElement("div");
+
+        this.domElementFood.className = "food";
+        this.domElementFood.style.width = this.width + "vw";
+        this.domElementFood.style.height = this.height + "vh";
+        this.domElementFood.style.left = this.positionX + "vw";
+        this.domElementFood.style.bottom = this.positionY + "vh";
+
+        this.board = document.getElementById("board");
+        this.board.appendChild(this.domElementFood);
+    }
+
+}
+
+function createFoodAtRandom() {
+    let randomDelay = Math.floor(Math.random() * (12000 - 5000 + 1)) + 5000;
+    setTimeout(createFoodAtRandom, randomDelay);
+    const food = new Food();
+}
+
+createFoodAtRandom();
